bc9153c285d22fd8310fb5d4148332d4fd6b3163
Diego Da'Silva <smartcrashomg@gmail.com>
2022-04-22 10:29:31 -0400

    Disable Laravel Mix notifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require("laravel-mix");
 
 mix.ts("resources/js/app.tsx", "public/js")
     .react()
-    .postCss("resources/css/app.css", "public/css", []);
+    .postCss("resources/css/app.css", "public/css", [])
+    .disableNotifications();
