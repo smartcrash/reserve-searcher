06478d95909c1a11eef48697da5862f222efd057
Diego Da'Silva <smartcrashomg@gmail.com>
2022-04-22 13:47:33 -0400

    Create constants.tsx

--- resources/js/Helper/toCurrency.tsx
+++ resources/js/Helper/toCurrency.tsx
@@ -1,5 +1,7 @@
+import { CURRENCY } from "../constants";
+
 export const toCurrency = (amount: number) =>
     new Intl.NumberFormat("de-DE", {
         style: "currency",
-        currency: "EUR",
+        currency: CURRENCY,
     }).format(amount);
---
+++ resources/js/constants.tsx
@@ -0,0 +1,2 @@
+export const MAX_BOOKING_DATE = new Date(2022, 11, 31);
+export const CURRENCY = "EUR";
