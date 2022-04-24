050ceb32771d89d4ddbf3637bffc7078e223f28d
Diego Da'Silva <smartcrashomg@gmail.com>
2022-04-24 09:15:14 -0400

    Add foot note

--- resources/js/Shared/Footer.tsx
+++ resources/js/Shared/Footer.tsx
@@ -1,5 +1,17 @@
+import { Box, Container, Text } from "@chakra-ui/react";
 import React from "react";
 
 export const Footer = () => {
-    return <></>;
+    return (
+        <Box py={"5"} mt={"2"} bg={"gray.200"}>
+            <Container maxW={"container.xl"}>
+                <Text textAlign={"right"} color={"gray.500"}>
+                    Made with ❤️ by{" "}
+                    <a href="https://github.com/smartcrash" target={"_blank"}>
+                        Diego
+                    </a>
+                </Text>
+            </Container>
+        </Box>
+    );
 };
